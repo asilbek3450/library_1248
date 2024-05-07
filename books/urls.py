a674a3fae80f4aaa741b706e132674e2d5254926
asilbek <asilbekmirolimov@gmail.com>
2024-05-07 18:15:36 +0500

    changes

--- books/urls.py
+++ books/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path, include
-from .views import BookListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
-from .views import BookListCreateAPIView, BookRetrieveUpdateDestroyAPIView
+# from .views import BookListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
+# from .views import BookListCreateAPIView, BookRetrieveUpdateDestroyAPIView
+from .views import BookList
 
 urlpatterns = [
 
-    path('books/', BookListView.as_view()),
-    path('books/create/', BookCreateView.as_view()),
-    path('books/<int:pk>/', BookDetailView.as_view()),
-    path('books/update/<int:pk>/', BookUpdateView.as_view()),
-    path('books/delete/<int:pk>/', BookDeleteView.as_view()),
+    # path('books/', BookListView.as_view()),
+    # path('books/create/', BookCreateView.as_view()),
+    # path('books/<int:pk>/', BookDetailView.as_view()),
+    # path('books/update/<int:pk>/', BookUpdateView.as_view()),
+    # path('books/delete/<int:pk>/', BookDeleteView.as_view()),
+    #
+    # path('bookscreate/', BookListCreateAPIView.as_view()),
+    # path('booksignle/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view()),
 
-    path('bookscreate/', BookListCreateAPIView.as_view()),
-    path('booksignle/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view()),
-
-
+    path('books/', BookList.as_view()),
 ]
--- books/views.py
+++ books/views.py
@@ -3,6 +3,9 @@ from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView,
     ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Book
 from .serializers import BookListSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -12,39 +15,100 @@ from .serializers import BookListSerializer
 # abstraction, inheritance, polymorphism, encapsulation
 # abstraksiya, merosxorlik, polimorfizm, inkapsulyatsiya
 
-class BookListView(ListAPIView):
-    queryset = Book.objects.all()  # type: list
-    serializer_class = BookListSerializer
-
-
-class BookCreateView(CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookListSerializer
-
-
-class BookDetailView(RetrieveAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookListSerializer
-
-
-class BookUpdateView(UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookListSerializer
-
-
-class BookDeleteView(DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookListSerializer
-
-
-class BookListCreateAPIView(ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookListSerializer
-
-
-class BookRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookListSerializer
+# class BookListView(ListAPIView):  # get
+#     queryset = Book.objects.all()  # type: list
+#     serializer_class = BookListSerializer
+#
+#
+# class BookCreateView(CreateAPIView):  # post
+#     queryset = Book.objects.all()
+#     serializer_class = BookListSerializer
+#
+#
+# class BookDetailView(RetrieveAPIView):  # get
+#     queryset = Book.objects.all()
+#     serializer_class = BookListSerializer
+#
+#
+# class BookUpdateView(UpdateAPIView):  # put, patch
+#     queryset = Book.objects.all()
+#     serializer_class = BookListSerializer
+#
+#
+# class BookDeleteView(DestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookListSerializer
+#
+#
+# class BookListCreateAPIView(ListCreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookListSerializer
+#
+#
+# class BookRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):  # get, put, patch, delete
+#     queryset = Book.objects.all()
+#     serializer_class = BookListSerializer
 
 
 # CRUD - Create, Retrieve(Read), Update, Delete
+
+# concrete views
+# 1. ListAPIView
+# 2. CreateAPIView
+# 3. RetrieveAPIView
+# 4. UpdateAPIView
+# 5. DestroyAPIView
+# 6. ListCreateAPIView
+# 7. RetrieveUpdateDestroyAPIView
+
+# class based views
+# 1. APIView
+# 2. ModelViewSet
+# 3. GenericAPIView
+# 4. ViewSet
+# 5. GenericViewSet
+# 6. ReadOnlyModelViewSet
+
+
+class BookList(APIView):
+
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookListSerializer(books, many=True)
+        data = {
+            'status': f"{len(books)} ta kitob mavjud",
+            'kitoblar': serializer.data
+        }
+        return Response(data)
+
+    def post(self, request):
+        data = request.data
+        serializer = BookListSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                'status': 'Kitob muvaffaqiyatli yaratildi',
+                'kitob': serializer.data
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookDetail(APIView):
+    """
+    Retrieve, update or delete a snippet instance.
+    """
+
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def patch(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
+
